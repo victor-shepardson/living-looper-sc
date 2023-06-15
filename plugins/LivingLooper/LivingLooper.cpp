fcde4a2c481916061945c8023e6ef0dc1bc06513
Victor Shepardson <victor.shepardson@gmail.com>
2023-06-15 17:57:25 +0000

    sample-rate and block-size independence

--- plugins/LivingLooper/LivingLooper.cpp
+++ plugins/LivingLooper/LivingLooper.cpp
@@ -9,12 +9,7 @@ static InterfaceTable* ft;
 
 namespace LivingLooper {
 
-auto LivingLooper::models = std::map<std::string, LLModel* >();
-
 LivingLooper::LivingLooper() {
-    inIdx = outIdx = 0;
-    first_block_done = false;
-
     filename_length = in0(0);
     // std::cout<<filename_length<<std::endl;
     // char path[filename_length];
@@ -23,105 +18,43 @@ LivingLooper::LivingLooper() {
         path[i] = static_cast<char>(in0(i+1));
     }
 
-    auto kv = models.find(path);
-    if (kv==models.end()){
-        model = new LLModel();
-        std::cout << "loading: \"" << path << "\"" << std::endl;
-        model->load(path);
-        models.insert({path, model});
-    } else {
-        model = kv->second;
-        std::cout << "found \"" << path << "\" already loaded" << std::endl;
-    }
-
-    model->reset();
-
-    int block_bytes = model->block_size * sizeof(float);
-    inBuffer = (float*)RTAlloc(this->mWorld, block_bytes);
-    outBuffer = (float*)RTAlloc(this->mWorld, model->n_loops * block_bytes);
+    model = std::make_unique<LLModel>(mWorld->mFullRate.mSampleRate);
+    model->load(path);
 
     mCalcFunc = make_calc_function<LivingLooper, &LivingLooper::next>();
-
-    this->ugen_outputs = model->n_loops;
 }
 
-LivingLooper::~LivingLooper() {
-    RTFree(this->mWorld, inBuffer);
-    RTFree(this->mWorld, outBuffer);
-}
+// LivingLooper::~LivingLooper() {
+// }
 
 void LivingLooper::write_zeros_kr() {
-    // std::cout<<"write zeros"<<std::endl;
-    for (int j=0; j < this->ugen_outputs; ++j){
-        out0(j) = 0;
-    }
+    RANGE(j, mNumOutputs) out0(j) = 0;
 }
 
 void LivingLooper::write_zeros_ar(int i) {
-    // std::cout<<"write zeros"<<std::endl;
-    for (int j=0; j < this->ugen_outputs; ++j){
-        out(j)[i] = 0;
-    }
+    RANGE(j, mNumOutputs) out(j)[i] = 0;
 }
 
 void LivingLooper::next(int nSamples) {
+    float buf[MAX_LOOPS];
+
     const float* input = in(filename_length+1);
     const int loop_idx = in0(filename_length+2);
     const int oneshot = in0(filename_length+3);
 
-    int n_loops = model->n_loops;
-    int model_block = model->block_size;
-    int host_block = nSamples;
+    model->loop_idx = loop_idx;
+    model->oneshot = oneshot;
 
-    // assume model_block and host_block are powers of two
-    // handle case when model_block > host_block and the reverse
-    int io_blocks = ceil(float(host_block) / model_block);
-    int min_block = std::min(model_block, host_block);
+    if (!model->loaded){
+        RANGE(i, nSamples) write_zeros_ar(i);
+        return;
+    }
 
-    int hostInIdx = 0;
-    int hostOutIdx = 0;
-
-    // std::cout << io_blocks << std::endl;
-
-    for (int block = 0; block < io_blocks; ++block){
-
-        int i;
-        for (i = 0; i < min_block; ++i) {
-            inBuffer[inIdx] = input[hostInIdx];
-            hostInIdx++;
-            inIdx++;
-            if(inIdx == model_block){
-                //process block
-                // std::cout << "forward call" << std::endl;
-                model->forward(inBuffer, loop_idx, oneshot, outBuffer);
-                outIdx = inIdx = 0;
-                first_block_done = true;
-            }
+    RANGE(i, nSamples) {
+        model->step(input[i], buf);
+        RANGE(j, model->n_loops) {
+            out(j)[i] = buf[j];
         }
-        // std::cout << "in samples:" << i << std::endl;
-        // std::cout << "inIdx is:" << inIdx << std::endl;
-
-        for (i = 0; i < min_block; ++i) {
-            if (model->loaded && first_block_done){
-                for (int j=0; j<n_loops; j++){
-                    if (outIdx >= model_block*n_loops) {
-                        std::cout<<"indexing error"<<std::endl;
-                        outIdx = 0;
-                    }
-                    // out(j)[i] = outBuffer[outIdx*n_loops + j];
-                    out(j)[hostOutIdx] = outBuffer[outIdx];
-                    outIdx++;
-                }
-                hostOutIdx++;
-            }
-            else {
-                write_zeros_ar(i);
-            }
-        }
-        // std::cout << "out samples:" << i << std::endl;
-        // std::cout << "outIdx is:" << outIdx << std::endl;
-
-
     }
 }
 } // namespace LivingLooper
--- plugins/LivingLooper/LivingLooper.hpp
+++ plugins/LivingLooper/LivingLooper.hpp
@@ -19,143 +19,19 @@
 
 namespace LivingLooper {
 
-// LLModel encapsulates the libtorch parts
-// struct LLModel {
-
-//   torch::jit::Module model;
-
-//   int sr;
-//   int block_size;
-//   int z_per_second;
-//   int latent_size;
-//   int n_loops;
-//   bool loaded;
-    
-//   std::vector<torch::jit::IValue> inputs_rave;
-//   std::vector<torch::jit::IValue> inputs_empty;
-
-//   LLModel() {
-//     // at::init_num_threads();
-//     // at::set_num_threads(1);
-//     // torch::set_num_threads(1);
-
-// //    unsigned int num_threads = std::thread::hardware_concurrency();
-//     this->loaded=false;
-//     torch::jit::getProfilingMode() = false;
-//     c10::InferenceMode guard;
-//     torch::jit::setGraphExecutorOptimize(true);
-//     }
-    
-//   void load(const std::string& rave_model_file) {
-//     // std::cout << "\"" <<rave_model_file << "\"" <<std::endl;
-//     try {
-//         c10::InferenceMode guard;
-//         this->model = torch::jit::load(rave_model_file);
-//         this->model.eval();
-//         // this->model = torch::jit::optimize_for_inference(this->model);
-//     }
-//     catch (const c10::Error& e) {
-//       // why no error when filename is bad?
-//         std::cout << e.what();
-//         std::cout << e.msg();
-//         std::cout << "error loading the model\n";
-//         return;
-//     }
-
-//     // support for Neutone models
-//     // if (this->model.hasattr("model")){
-//     //   this->model = this->model.attr("model").toModule();
-//     // }
-
-//     this->block_size = this->latent_size = this->sr = this->n_loops = -1;
-
-//     for (auto const& attr: model.named_attributes()) {
-//         if (attr.name == "n_loops") {this->n_loops = attr.value.toInt();} 
-//         if (attr.name == "block_size") {this->block_size = attr.value.toInt();} 
-//         if (attr.name == "sampling_rate") {this->sr = attr.value.toInt();} 
-//     }
-//     // this->z_per_second = this->sr / this->block_size;
-
-//     if ((this->block_size<=0) || 
-//         (this->n_loops<=0) || 
-//         // (this->latent_size<0) || 
-//         (this->sr<=0)){
-//       std::cout << "model load failed" << std::endl;
-//       return;
-//     }
-
-//     std::cout << "\tnumber of loops: " << this->n_loops << std::endl;
-//     std::cout << "\tblock size: " << this->block_size << std::endl;
-//     // std::cout << "\tlatent size: " << this->latent_size << std::endl;
-//     std::cout << "\tsample rate: " << this->sr << std::endl;
-
-//     c10::InferenceMode guard;
-//     inputs_rave.clear();
-//     inputs_rave.push_back(torch::IValue(0));
-//     inputs_rave.push_back(torch::ones({1,1,block_size}));
-//     inputs_rave.push_back(torch::IValue(0));
-
-//     this->loaded = true;
-//   }
-
-//   void reset () {
-//     c10::InferenceMode guard;
-//     this->model.get_method("reset")(inputs_empty);
-//   }
-
-//   void forward (float* input, int loop_idx, int oneshot, float* outBuffer) {
-//     c10::InferenceMode guard;
-
-//     inputs_rave[0] = torch::IValue(loop_idx); 
-
-//     inputs_rave[1] = torch::from_blob(
-//       input, block_size).reshape({1, 1, block_size});
-
-//     inputs_rave[2] = torch::IValue(oneshot); 
-
-//     // auto t_start = std::chrono::high_resolution_clock::now();
-//     const auto y = this->model(inputs_rave).toTensor();
-//     // auto elapsed = std::chrono::high_resolution_clock::now() - t_start;
-// 		// std::cout << "model time: " << elapsed.count() * 1e-6 << " ms" << std::endl;
-
-//     auto acc = y.accessor<float, 3>();
-//     for(int j=0; j<acc.size(0); j++) {
-//       for(int i=0; i<acc.size(2); i++) {
-//         outBuffer[n_loops*i + j] = acc[j][0][i];
-//       }
-//     }
-//     // auto data = y.data_ptr<float>(); //n_loops x 1 x block_size
-//     // for (int i=0; i<block_size*n_loops; i++){
-//     //   outBuffer[i] = data[i];
-//     // }  
-//   }
-
-// };
-
 class LivingLooper : public SCUnit {
 
   public:
     LivingLooper();
-    ~LivingLooper();
+    // ~LivingLooper();
 
     void write_zeros_kr();
     void write_zeros_ar(int i);
     void next(int nSamples);
 
-    LLModel * model;
-    static std::map<std::string, LLModel* > models;
+    std::unique_ptr<LLModel> model;
 
-    float * inBuffer; 
-    size_t inIdx;
-
-    float * outBuffer;
-    size_t outIdx;
-
-    bool first_block_done;
     int filename_length;
-    int ugen_inputs;
-    int ugen_outputs;
-
 };
 
 } // namespace LivingLooper
